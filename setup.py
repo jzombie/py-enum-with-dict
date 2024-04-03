0aa88814f4c576878f5f4390b7a94df663369140
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-04-03 13:40:41 -0500

    Remove Python 3.6 from setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -25,6 +24,6 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     keywords='enum python utilities enum-to-dict enum-with-dict',
 )
