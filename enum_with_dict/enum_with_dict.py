87f1dfaece1f7ac90e4956cd7ea072fbbd0a9a96
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-04-03 14:32:04 -0500

    Add types

--- enum_with_dict/enum_with_dict.py
+++ enum_with_dict/enum_with_dict.py
@@ -1,18 +1,20 @@
 from enum import Enum
+from typing import Any, ClassVar, Dict, Type
+
 
 class EnumWithDict(Enum):
     @classmethod
-    def to_dict(cls):
+    def to_dict(cls) -> Dict[str, Any]:
         """Return a dictionary representation of the enum."""
         return {member.name: member.value for member in cls}
 
     @classmethod
-    def get_initial(cls):
+    def get_initial(cls) -> Any:
         """Get the first value from the enum dictionary."""
         return next(iter(cls.to_dict().values()))
 
     @classmethod
-    def get(cls, key, default=None):
+    def get(cls, key: str, default: Any = None) -> Any:
         """Get the value for the given key or return default or initial value."""
         # If default is not provided, use the initial value
         if default is None:
@@ -20,7 +22,7 @@ class EnumWithDict(Enum):
         return cls.to_dict().get(key, default)
 
     @classmethod
-    def validate_mapping(cls, mapping):
+    def validate_mapping(cls, mapping: Dict[Any, Any]) -> bool:
         """Validate that every enum member has a mapping, raise error if any are missing."""
         missing = [member for member in cls if member.value not in mapping]
         if missing:
