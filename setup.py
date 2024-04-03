98b2ff74b95841b7cb5a725328349db8e66e7c5f
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-04-03 13:43:42 -0500

    Bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='enum_with_dict',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     author='Jeremy Harris',
     author_email='jeremy.harris@zenosmosis.com',
